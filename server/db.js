6a848f288f30f8a5bb2aea3e497b6d4e6edfe0e9
Scott Donnelly <scott.donnelly@bskyb.com>
2015-05-20 12:08:52 +0100

    db: use node URL formatter

--- server/db.js
+++ server/db.js
@@ -1,19 +1,18 @@
 'use strict';
-var monk = require('monk'),
+var url = require('url'),
+    monk = require('monk'),
     wrap = require('co-monk');
 
 module.exports = function(config) {
 
-    var uristring = ['mongodb://'];
-    if (config.user.length) { uristring.push(`${config.user}:${config.pass}@`); }
-    uristring.push(config.host);
-    if (config.port) { uristring.push(`:${config.port}`) }
-    uristring.push(`/${config.db}`);
+    var uri = { protocol: 'mongodb', slashes: true, hostname: config.host, pathname: config.db };
+    if (config.user) { uri.auth = `${config.user}:${uri.pass}`; }
+    if (config.port) { uri.port = config.port; }
 
-    var db = monk(uristring.join());
+    var db = monk(url.format(uri));
 
     wrap(db.get('post')).find({}, err =>
-        err ? console.error(`Could not connect to DB ${uristring} (${err})`) : console.info('Connected to DB')
+        err ? console.error(`Could not connect to DB ${uri.db} (${err})`) : console.info('Connected to DB')
     );
 
     return db;
